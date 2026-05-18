c845271aa2e80de5ca4dca5cc5713bd020092c47
Anthony Wilson <awilsonprogramming@gmail.com>
2026-05-18 19:46:42 +1000

    Add basic command send/receive
    - Added command IDs (identical to the enum defined in https://github.com/AnthonyW2/qmk_firmware_fw16/blob/custom-macropad/keyboards/framework/macropad/keymaps/anthonyw/keymap.c).
    - Added send_command function to more cleanly send a RAW HID command.
    - Added read_interface and handle_custom_hid functions to process incoming RAW HID messages.

--- macropad_daemon.py
+++ macropad_daemon.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import hid
+from enum import Enum
 
 # Framework Laptop 16 RGB Macropad
 macropad_vid = 0x32ac
@@ -15,6 +16,39 @@ report_length = 32
 
 # Custom command namespace
 custom_hid_prefix = 0xFF
+
+# Amount of time to wait for each read to complete
+read_timeout = 1000
+
+"""
+Define the codes for the custom RAW HID commands (identical to the custom_hid_commands enum in firmware)
+"""
+# Send/receive a ping
+hid_cmd_ping       = 0x00
+# Acknowledge a ping
+hid_cmd_ack        = 0x01
+
+# Send/receive new layer
+hid_cmd_set_layer  = 0x02 # [layer]
+
+# Receive a new RGB value for a key
+hid_cmd_set_rgb    = 0x03 # [key id, r, g, b]
+# Receive a new overall brightness level
+hid_cmd_set_bright = 0x04 # [brightness divisor]
+# RGB matrix enable/disable & set mode
+hid_cmd_rgb_matrix = 0x05 # [new state, new mode]
+
+# Send a key downpress event
+hid_cmd_key_down   = 0x06 # [key id]
+# Send a key release event
+hid_cmd_key_up     = 0x07 # [key id]
+
+# Ask the daemon for host system status
+hid_cmd_status_req = 0x08
+# Receive host system status response
+hid_cmd_status_res = 0x09 # [mem, cpu, gpu, gui, crashes, kernel]
+""" End custom_hid_commands """
+
 
 # Store the index of the key at each physical position
 key_indices_2d_map = (
@@ -83,30 +117,78 @@ def get_raw_hid_interface(vid, pid, usage_page = 0xFF60, usage_id = 0x61):
     
     return interface
 
-def send_raw_report(interface, data):
+def send_command(interface, command_id, data):
     """
-    Send a RAW HID report over the given interface.
+    Send a RAW HID command over the given interface.
     
     :param interface: RAW HID handle for the keyboard.
     
-    :param data: Raw data to send.
-    """
-    request_data = [0x00] * (report_length + 1) # First byte is Report ID
-    request_data[1:len(data) + 1] = data
-    request_report = bytes(request_data)
+    :param command_id: Identifier of the command.
     
-    print("Request:")
-    print(request_report)
+    :param data: Raw data to send (should be of length report_length-2).
+    """
+    
+    # Ensure the length of the data is correct
+    assert len(data) <= report_length - 2
+    
+    # Create the report contents
+    # First byte is Report ID
+    request_data = [0x00] * (report_length + 1)
+    # Use our custom command prefix
+    request_data[1] = custom_hid_prefix
+    # Insert the command ID
+    request_data[2] = command_id
+    # Add any extra data that the command sends
+    request_data[3:len(data) + 3] = data
     
     try:
-        interface.write(request_report)
-        
-        response_report = interface.read(report_length, timeout=1000)
-        
-        print("Response:")
-        print(response_report)
+        interface.write(bytes(request_data))
     except:
-        print("Failed to send report")
+        print("ERROR: Failed to send report with command ID "+str(command_id))
+
+def read_interface(interface, timeout):
+    """
+    Read a message from the keyboard, if there is one.
+    
+    :param interface: RAW HID handle for the keyboard.
+    
+    :param timeout: Amount of time (in milliseconds) to wait for a message.
+    """
+    
+    report_data = interface.read(report_length, timeout=timeout)
+    
+    if len(report_data) > 0:
+        handle_custom_hid(report_data)
+
+def handle_custom_hid(data):
+    """
+    Handle a RAW HID message from the macropad.
+    
+    :param data: RAW HID report, with length report_length.
+    """
+    
+    if len(data) != report_length:
+        print("WARNING: Message length ("+len(data)+") not equal to expected report length, skipping.")
+        return
+    
+    namespace = data[0]
+    if namespace != custom_hid_prefix:
+        print("WARNING: Intercepted a report which was not meant for us:", data)
+    
+    command_id = data[1]
+    command_data = data[2:]
+    
+    if command_id == hid_cmd_ping:
+        # Ping received, need to acknowledge
+        print("Ping received")
+        
+    elif command_id == hid_cmd_ack:
+        # Ping acknowledged, macropad must be available
+        print("Ping acknowledged")
+        
+    else:
+        # Not a known command
+        print("WARNING: Unknown command with ID "+str(command_id))
 
 def coords_to_key(x, y):
     """
@@ -141,10 +223,10 @@ if __name__ == '__main__':
         sys.exit(1)
     
     # Send a ping
-    send_raw_report(macropad_interface, [
-        custom_hid_prefix,
-        0x00
-    ])
+    send_command(macropad_interface, hid_cmd_ping, [])
+    
+    while True:
+        read_interface(macropad_interface, read_timeout)
     
     macropad_interface.close()
 
